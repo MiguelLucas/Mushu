8e6a1d5e24d6bbf9dd2e72f1770d013bf99ff96b
Miguel Lucas <cmiguel.lucas@gmail.com>
2024-09-23 21:56:39 +0100

    Added settings menu

--- app/app/src/main/java/com/mlucas/mushu/MainActivity.kt
+++ app/app/src/main/java/com/mlucas/mushu/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.mlucas.mushu
 
 import android.Manifest.permission.POST_NOTIFICATIONS
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -49,6 +52,23 @@ class MainActivity : AppCompatActivity() {
 
         askNotificationPermission()
         configureNotifications()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                // Handle the settings action
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     // Declare the launcher at the top of your Activity/Fragment:
