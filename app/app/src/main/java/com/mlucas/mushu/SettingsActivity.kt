df4e068df8f05ee63ffba785c2aae1675edf061c
Miguel Lucas <cmiguel.lucas@gmail.com>
2024-11-25 20:04:23 +0000

    Added options for enabling alarm, setting alarm max time and max number of notifications

--- app/app/src/main/java/com/mlucas/mushu/SettingsActivity.kt
+++ app/app/src/main/java/com/mlucas/mushu/SettingsActivity.kt
@@ -1,8 +1,14 @@
 package com.mlucas.mushu
 
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -22,5 +28,12 @@ class SettingsActivity : AppCompatActivity() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
         }
+    }
+
+    companion object {
+        const val NAME = "settings"
+        const val NOTIFICATIONS_MAX_NUMBER = "notifications_max_number"
+        const val ALARM_ENABLED = "alarm_enabled"
+        const val ALARM_MAX_TIME_TO_PLAY = "alarm_max_time_to_play"
     }
 }
