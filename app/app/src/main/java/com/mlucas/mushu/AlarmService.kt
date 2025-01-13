aa0bbc6093a97204accbb85a9c08d07bc0348c0c
Miguel Lucas <cmiguel.lucas@gmail.com>
2025-01-13 18:10:33 +0000

    Added (commented) wake lock and setting visibility on alarm notification

--- app/app/src/main/java/com/mlucas/mushu/AlarmService.kt
+++ app/app/src/main/java/com/mlucas/mushu/AlarmService.kt
@@ -5,12 +5,14 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.media.RingtoneManager
 
 import android.os.CountDownTimer
 import android.os.IBinder
+import android.os.PowerManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.preference.PreferenceManager
@@ -19,6 +21,7 @@ import androidx.preference.PreferenceManager
 class AlarmService : Service() {
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var timer: CountDownTimer
+    private lateinit var wakeLock: PowerManager.WakeLock
 
     private var timeToStopAlarm: Long = 60
     private var alarmEnabled: Boolean = true
@@ -43,6 +46,13 @@ class AlarmService : Service() {
             stopSelf()
             return START_NOT_STICKY
         }
+
+        //val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        //wakeLock = powerManager.newWakeLock(
+        //    PowerManager.PARTIAL_WAKE_LOCK,
+        //    "Mushu::AlarmWakeLock"
+        //)
+        //wakeLock.acquire(10 * 60 * 1000L) //10 minutes
 
         if (intent?.action == "START_ALARM") {
             val title = intent.getStringExtra("title") ?: "Mushu Alert"
@@ -89,6 +99,11 @@ class AlarmService : Service() {
             timer.cancel()
         }
 
+        // Release the wake lock if it exists
+        if (::wakeLock.isInitialized && wakeLock.isHeld) {
+            wakeLock.release()
+        }
+
         // Stop the foreground service
         stopForeground(true)
         stopSelf()
@@ -108,6 +123,7 @@ class AlarmService : Service() {
             .setContentText(message)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setCategory(NotificationCompat.CATEGORY_ALARM)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
             .addAction(android.R.drawable.ic_dialog_alert, "Stop", stopPendingIntent)
             .setAutoCancel(true)
             .setDeleteIntent(stopPendingIntent)
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,5 +9,5 @@ plugins {
 }
 
 
-val appVersion by extra { "0.2.1-rc.3" }
+val appVersion by extra { "0.2.1-rc.4" }
 val roomVersion by extra { "2.6.1" }
